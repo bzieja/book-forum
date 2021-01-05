06db396ec222007b2bf447a7a105cbf70441f437
bzieja <56655333+bzieja@users.noreply.github.com>
2021-01-05 15:00:08 +0100

    initial commit

---
+++ src/main/java/pl/bzpb/bookforum/BookForumApplication.java
@@ -0,0 +1,13 @@
+package pl.bzpb.bookforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bzpb/bookforum/BookForumApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bzpb.bookforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
