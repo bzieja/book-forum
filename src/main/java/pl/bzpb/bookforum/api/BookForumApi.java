da97cc1e551138961e7c03ee7d600f93e0f8e2ff
pazindor <xxpazindorxx@gmial.com>
2021-01-05 15:16:20 +0100

    Create BookForumApi.java

---
+++ src/main/java/pl/bzpb/bookforum/api/BookForumApi.java
@@ -0,0 +1,9 @@
+package pl.bzpb.bookforum.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class BookForumApi {
+}
